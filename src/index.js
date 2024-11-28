225b7e365bf04e6fa81ce0b2065a85f6e84bef90
KVG24 <141866132+KVG24@users.noreply.github.com>
2024-11-28 10:00:50 +0000

    Add html skeleton

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import "./style.css";
 
 const searchInput = document.getElementById("search");
 const submitBtn = document.getElementById("submit");
+const tempCurrent = document.getElementById("tempcurrent");
+const tempMax = document.getElementById("tempmax");
+const tempMin = document.getElementById("tempmin");
+const addressText = document.getElementById("location");
+const desc = document.getElementById("description");
 
 submitBtn.addEventListener("click", (event) => {
     event.preventDefault();
     fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchInput.value}/next7days?unitGroup=us&key=XSQJBABTWYL3YQLFB8ZKKY599`,
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchInput.value}/next7days?unitGroup=metric&key=XSQJBABTWYL3YQLFB8ZKKY599`,
         {
             method: "GET",
             headers: {},
@@ -15,6 +20,14 @@ submitBtn.addEventListener("click", (event) => {
         .then((response) => {
             const data = response.json();
             console.log(data);
+            return data;
+        })
+        .then((data) => {
+            addressText.textContent = data.resolvedAddress;
+            desc.textContent = data.description;
+            tempCurrent.textContent = data.currentConditions.temp;
+            tempMax.textContent = `Max ${data.days[0].tempmax}`;
+            tempMin.textContent = `Min ${data.days[0].tempmin}`;
         })
         .catch((err) => {
             console.error(err);
